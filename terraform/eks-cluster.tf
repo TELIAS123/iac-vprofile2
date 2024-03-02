f61766ec2c52f183ba219da9767e7020e56d9caa
devop4sure1 <devops4sure1@gmail.com>
2024-03-02 13:42:38 -0700

    update s3 region

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -21,7 +21,7 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 1
+      max_size     = 2
       desired_size = 1
     }
 
@@ -31,7 +31,7 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 1
+      max_size     = 2
       desired_size = 1
     }
   }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "bucket-for-github-teddy"
+    bucket = "bucket-for-github-ohio"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
